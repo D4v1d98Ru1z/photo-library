d8e908dc50e71f3b740c261099dedd0b791c5765
David Ruiz <davidl.ruizc@gmail.com>
2019-03-17 19:46:03 -0500

    Helper which call unsplash api

---
+++ src/api/api.js
@@ -0,0 +1,10 @@
+// axios to make http request to api
+import axios from 'axios'
+
+// New axios instance to Authorization with unsplash
+export default axios.create({
+    baseURL: 'https://api.unsplash.com',
+    headers: {
+        Authorization: 'Client-ID 819c8546ef55793abf0167687635782a167b079a73d258c8a8a3aa4d6238aa48'
+    }
+})
