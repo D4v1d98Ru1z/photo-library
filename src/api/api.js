c2984c6dfdab2731543045df0c24677b531278b7
David Ruiz <davidl.ruizc@gmail.com>
2019-03-19 00:30:17 -0500

    Adding comments

--- src/api/api.js
+++ src/api/api.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
+        // Unsplash Access Key
         Authorization: 'Client-ID 819c8546ef55793abf0167687635782a167b079a73d258c8a8a3aa4d6238aa48'
     }
 })
