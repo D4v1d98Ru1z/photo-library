367ea4d59da3283d48405aa135c3aa9a4b1adb9c
David Ruiz <davidl.ruizc@gmail.com>
2019-03-18 21:41:13 -0500

    Search bar change the form state

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -2,12 +2,36 @@ import React, { Component } from 'react'
 import './SearchBar.css'
 
 export default class SearchBar extends Component {
+  // Store the input value
+  state = {
+    input: ''
+  }
+
+  // Change the form state 
+  hangleChange(event) {
+    this.setState({
+      input: event.target.value
+    })
+  }
+
+  // Submit the event
+  handleSubmit(event){
+    // disable the native event
+    event.preventDefault()
+    this.props.handleSubmit(this.state.input)
+  }
   render() {
     return (
       <div className="Nav">
         <nav className="SearchBar">
-            <form>
-                <input placeholder="Search" className="Search"/>
+            <form onSubmit={this.handleSubmit}>
+                <input 
+                  type="text"
+                  placeholder="Search" 
+                  className="Search"
+                  value={this.state.input}
+                  onChange={this.hangleChange}
+                />
             </form>
         </nav>
       </div>
