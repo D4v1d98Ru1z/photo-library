cc6d37401115003998fdab1a75b7d0e67537952c
David Ruiz <davidl.ruizc@gmail.com>
2019-03-20 04:54:27 -0500

    Got a bug here trying to figure it out the rows with grid

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react'
 import './ImageCard.css'
 
 export default class ImageCard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      gridSpan: 0
+    }
+    this.picRef = React.createRef()
+  }
+  
+
+  setGridSpan = () => {
+    const height = this.picRef.current.clientHeight
+    console.log(`Height from setGridSpan ${height}`)
+    const gridSpan = height + 1
+    console.log(`gridSpan from setGridSpan ${gridSpan}`)
+    this.setState({
+      gridSpan
+    })
+  }
+
+  componentDidMount(){
+    this.picRef.current.addEventListener('load', this.setGridSpan)
+  }
+
   render() {
     return (
-      <div className="item">
+      <div 
+        className="item"
+        style={{ gridRowEnd: `span ${this.state.gridSpan}` }}
+      >
         <img
+          ref={this.picRef}
           src={this.props.img.urls.regular}
           alt={this.props.img.description}
         />
