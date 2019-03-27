5ee0e31dc24f4ad96296c7f39f491c06a6716b85
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2019-03-26 20:20:25 -0500

    Re Refactor of post

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PostOverlay from './PostOverlay'
 import './ImageCard.css'
 
 export default function Post(props) {
@@ -9,26 +10,9 @@ export default function Post(props) {
         src={props.img.urls.regular}
         alt={props.img.description}
       />
-      <div className="post-overlay">
-        <p>
-          <span className="overlay-btn btn-1">
-            <i className="far fa-heart"></i>
-            <span>
-              {
-                props.img.user.total_likes
-              }
-            </span>
-          </span>
-          <span className="overlay-btn btn-2">
-            <i className="far fa-images"></i>
-            <span>
-              {
-                props.img.user.total_photos
-              }
-            </span>
-          </span>
-        </p>
-      </div>
+      <PostOverlay
+        img={props.img}
+      />
     </div>
   )
 }
